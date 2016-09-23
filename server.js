6e1053d77b5c7ac03f6ce81766a82c2e7f3f2a45
Preeti-Nagal <parul10p13@gmail.com>
2016-09-23 18:36:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,54 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleone={
+    title:'article one/preeti nagal',
+    heading:'article one',
+    content:`
+           <p>
+                this is article one page of my web app
+            </p>`
+};
+
+function createTemplate(data){
+var title=data.title;
+var content=data.content;
+var heading=data.heading;
+var htmlTemplate=`
+<html>
+    <head>
+        <title>
+        ${title}
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+         <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    
+    <body>
+        <div class="container">
+          <div>
+            <a href="/">Home</a>
+          </div>
+          <hr/>
+          <h3>
+            ${heading}
+          </h3>
+          <div>
+            ${content}
+          </div>
+        </div>
+    </body>
+</html>
+`;
+return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(articleone)));
 });
 
 app.get('/article-two', function (req, res) {
